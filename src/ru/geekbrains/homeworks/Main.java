2ec46754483edc8effe3cd3d16ff812d60022e48
Kosenko <Kosenko.2000@yandex.ru>
2020-08-27 21:52:23 +0500

    lesson 2

--- src/ru/geekbrains/homeworks/Main.java
+++ src/ru/geekbrains/homeworks/Main.java
@@ -2,7 +2,35 @@ package ru.geekbrains.homeworks;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+	String [][] arr = {{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
+	try {
+        System.out.println(submit(arr));
+    }
+	catch (MyArraySizeException | MyArrayDataException e){
+	    e.printStackTrace();
+    }
+    }
+
+    public static int submit (String [][] arr) throws MyArraySizeException, MyArrayDataException{
+        int result = 0;
+        if (arr.length == 4 && arr[0].length == 4){
+            for (int i = 0; i<arr.length; i++){
+                for (int j = 0; j<arr[i].length; j++){
+                    if (isNumeric(arr[i][j])) {
+                        result += Integer.parseInt(arr[i][j]);
+                    } else {
+                        throw new MyArrayDataException("В массиве есть неверный элемент [" + i + ";" + j + "]");
+                    }
+                }
+            }
+            return result;
+        } else {
+            throw new MyArraySizeException("Передан неверный размер массива");
+        }
+    }
+    public static boolean isNumeric(String str)
+    {
+        return str.matches("-?\\d+(\\.\\d+)?");
     }
 }
